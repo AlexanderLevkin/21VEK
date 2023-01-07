e926612209b89df2a77806d4093ffa983de67b39
AlexanderLevkin <alexanlevkin@gmail.com>
2023-01-07 23:11:44 +0300

    21vek_ui_testing

--- BASE/base_class.py
+++ BASE/base_class.py
@@ -79,4 +79,4 @@ class Base:
     def get_screenshot(self):
         now_date = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
         name_screenshot = 'SHOT ' + now_date + '.png'
-        self.driver.save_screenshot('/Users/alexanderlevkin/Desktop/LESSONS/pythonProject/SCREEN' + name_screenshot)
+        self.driver.save_screenshot('/Users/alexanderlevkin/Desktop/LESSONS/pythonProject/SCREEN/' + name_screenshot)
